628e4aaf4c082075fe7ef20d40f8456d0be550a2
Benoît Vandanjon <benoit@vandanjon.fr>
2025-01-13 15:28:19 +0100

    [FEAT] : deny possibility to boat to move out of the map

--- server/src/router.ts
+++ server/src/router.ts
@@ -9,6 +9,7 @@ import tileActions from "./modules/tile/tileActions";
 router.get("/api/boats", boatActions.browse);
 router.post("/api/games", gameActions.add);
 router.get("/api/tiles", tileActions.browse);
-router.put("/api/boats/:id", boatActions.edit);
+
+router.put("/api/boats/:id", tileActions.validate, boatActions.edit);
 
 export default router;

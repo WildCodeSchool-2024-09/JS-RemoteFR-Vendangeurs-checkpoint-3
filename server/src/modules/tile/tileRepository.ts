cd72b9f5ae18b8de865e10e2e026a372a4591089
Foxrim <Flavien085@gmail.com>
2025-01-13 10:05:26 +0100

    [feat] - step 2

--- server/src/modules/tile/tileRepository.ts
+++ server/src/modules/tile/tileRepository.ts
@@ -12,12 +12,10 @@ type Tile = {
 
 class TileRepository {
   async readAll() {
-    // Execute the SQL SELECT query to retrieve all tiles from the "tile" table
     const [rows] = await databaseClient.query<Rows>(
       "select * from tile order by coord_y, coord_x",
     );
 
-    // Return the array of tiles
     return rows as Tile[];
   }
 

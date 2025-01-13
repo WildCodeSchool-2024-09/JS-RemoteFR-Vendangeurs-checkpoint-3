b6c62c8aee8b7463e2724d57a28e605c4ad11dd7
Laure Maffini <lauremaffini@gmail.com>
2025-01-13 12:34:46 +0100

    [feat] ok test install, 1 , et 2 et le 3 partiellement

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,11 @@
 import type { RequestHandler } from "express";
-
+import tileRepository from "./tileRepository";
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  // Fetch all tiles from the database
+  const tiles = await tileRepository.readAll();
+
+  // Respond with the tiles in JSON format
+  res.json(tiles);
 };
 
 const validate: RequestHandler = async (req, res, next) => {

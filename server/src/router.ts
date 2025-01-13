c7ef10eb83897040c15769af215b9205157e900c
Benoît Vandanjon <benoit@vandanjon.fr>
2025-01-13 15:01:02 +0100

    [FEAT] : possibility to move boat

--- server/src/router.ts
+++ server/src/router.ts
@@ -9,5 +9,6 @@ import tileActions from "./modules/tile/tileActions";
 router.get("/api/boats", boatActions.browse);
 router.post("/api/games", gameActions.add);
 router.get("/api/tiles", tileActions.browse);
+router.put("/api/boats/:id", boatActions.edit);
 
 export default router;

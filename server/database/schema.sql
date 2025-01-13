2cc6617e0e0cc7982193f6074ca43566235b7364
AURELIA GUARINO <aureliaguarino0@gmail.com>
2025-01-13 10:56:26 +0100

    [FEAT] step 1

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -6,11 +6,12 @@ CREATE TABLE boat (
   PRIMARY KEY(id)
 );
 
-CREATE TABLE tile (
+CREATE TABLE title (
   id INT AUTO_INCREMENT NOT NULL,
   type VARCHAR(255) NOT NULL,
   coord_x INT NOT NULL,
   coord_y INT NOT NULL,
+  has_treasure BOOLEAN NOT NULL DEFAULT FALSE,
   PRIMARY KEY(id)
 );
 
--- server/tests/step1.test.ts
+++ server/tests/step1.test.ts
@@ -7,21 +7,21 @@ import type { Rows } from "../database/client";
 
 describe("The has_treasure attribute", () => {
   test("you added the attribute in database/schema.sql (remember to run again db:migrate script)", async () => {
-    const [result] = await databaseClient.query<Rows>("describe tile");
+    const [result] = await databaseClient.query<Rows>("describe title");
 
     const hasTreasure = result.find(({ Field }) => Field === "has_treasure");
 
     expect(hasTreasure).toBeDefined();
   });
   test("the attribute is a boolean", async () => {
-    const [result] = await databaseClient.query<Rows>("describe tile");
+    const [result] = await databaseClient.query<Rows>("describe title");
 
     const hasTreasure = result.find(({ Field }) => Field === "has_treasure");
 
     expect(hasTreasure?.Type).toMatch(/tinyint/);
   });
   test("the attribute is false by default", async () => {
-    const [result] = await databaseClient.query<Rows>("describe tile");
+    const [result] = await databaseClient.query<Rows>("describe title");
 
     const hasTreasure = result.find(({ Field }) => Field === "has_treasure");
 

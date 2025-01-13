624568cf19db8039b2aa4a2df379dd6937080ec9
Benoît Vandanjon <benoit@vandanjon.fr>
2025-01-13 15:38:54 +0100

    [FEAT] : add informations on boat.readAll request

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -11,12 +11,17 @@ type Boat = {
 
 class BoatRepository {
   async readAll(where = {}) {
-    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await databaseClient.query<Rows>(
-      "select * from boat order by coord_y, coord_x",
+      `
+      SELECT boat.id, boat.coord_x, boat.coord_y, boat.name, tile.type, tile.has_treasure
+      FROM boat
+      JOIN tile
+      ON boat.coord_x = tile.coord_x
+      AND boat.coord_y = tile.coord_y
+      ORDER BY boat.coord_y, boat.coord_x
+      `,
     );
 
-    // Return the array of tiles
     return rows as Boat[];
   }
 

69be55a3dad518888e2b4e961ac752231019383b
Maxime TRICOCHE <maxime.trche@gmail.com>
2025-01-13 09:50:02 +0100

    step2 done

--- server/src/router.ts
+++ server/src/router.ts
@@ -10,9 +10,17 @@ import boatActions from "./modules/boat/boatActions";
 
 router.get("/api/boats", boatActions.browse);
 
+/* ************************************************************************* */
+
 import gameActions from "./modules/game/gameActions";
 
 router.post("/api/games", gameActions.add);
+
+/* ************************************************************************* */
+
+import tileActions from "./modules/tile/tileActions";
+
+router.get("/api/tiles", tileActions.browse);
 
 /* ************************************************************************* */
 

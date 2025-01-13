cbb963d1f8310ec23dbbe503529b55deeb53690d
Benoît Vandanjon <benoit@vandanjon.fr>
2025-01-13 14:32:16 +0100

    [FEAT] : possibility to read all tiles from DB

--- server/src/modules/tile/tileRepository.ts
+++ server/src/modules/tile/tileRepository.ts
@@ -12,12 +12,14 @@ type Tile = {
 
 class TileRepository {
   async readAll() {
-    // Execute the SQL SELECT query to retrieve all tiles from the "tile" table
     const [rows] = await databaseClient.query<Rows>(
-      "select * from tile order by coord_y, coord_x",
+      `
+      SELECT *
+      FROM tile
+      ORDER BY coord_y, coord_x
+      `,
     );
 
-    // Return the array of tiles
     return rows as Tile[];
   }
 
--- server/src/router.ts
+++ server/src/router.ts
@@ -2,18 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
-
 import boatActions from "./modules/boat/boatActions";
+import gameActions from "./modules/game/gameActions";
+import tileActions from "./modules/tile/tileActions";
 
 router.get("/api/boats", boatActions.browse);
-
-import gameActions from "./modules/game/gameActions";
-
 router.post("/api/games", gameActions.add);
-
-/* ************************************************************************* */
+router.get("/api/tiles", tileActions.browse);
 
 export default router;

95b60a4be780cbcdcde7833bc0e35feb0aa34840
jerem2802 <creamedoc@gmail.com>
2025-01-13 12:18:56 +0100

    [FEAT] resolve step2

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,13 @@
 import type { RequestHandler } from "express";
+import DatabaseClient from "../../../database/client";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const [rows] = await DatabaseClient.query("SELECT * FROM tile");
+    res.json(rows);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
